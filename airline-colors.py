0ecfee5c404848b01d1a23a769da6e546b2b8e49
Johan Kanflo <johan.kanflo@bitfuse.net>
2015-03-31 21:44:32 +0200

    Initial commit

---
+++ airline-colors.py
@@ -0,0 +1,181 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+
+
+import imagecolor
+import os
+import paho.mqtt.client as mosquitto
+import Queue
+import time
+from threading import *
+import remotelogger, logging, sys
+import socket
+import calendar, datetime
+import json
+import traceback
+import math
+import signal
+import random
+
+gQuitting = False
+gCurrentColor = ()
+gMaxDistance = 5000.0
+
+gMQTTBroker = "172.16.3.104"
+#gMQTTBroker = "gateway.local"
+gMQTTPort = "1883"
+
+log = logging.getLogger(__name__)
+
+
+def mqttOnConnect(mosq, obj, rc):
+    mosq.subscribe("adsb/proximity/json", 0)
+    log.debug("MQTT Connect: %s" % (str(rc)))
+
+def mqttOnDisconnect(mosq, obj, rc):
+    global gQuitting
+    log.debug("MQTT Disconnect: %s" % (str(rc)))
+    if not gQuitting:
+        while not mqttConnect():
+            time.sleep(10)
+            log.debug("Attempting MQTT reconnect")
+    log.debug("MQTT connected")
+
+def mqttOnMessage(mosq, obj, msg):
+    global gMaxDistance
+    global gCurrentColor
+    try:
+        data = json.loads(msg.payload)
+        airline = data["operator"]
+        distance = data["distance"]
+        lost = False
+        if "lost" in data:
+            lost = data["lost"]
+
+        if airline == "SAS":
+            airline = "SAS Airlines"
+
+        if lost:
+            log.debug("Lost sight of aircraft")
+            color = (0, 0, 0)
+        else:
+            color = imagecolor.getColor(airline)
+    #    print airline
+    #    if distance > gMaxDistance:
+    #      color[0] = color[1] = color[2] = 0
+    #    else:
+    #      color[0] = color[0] = 
+    #
+    #    color[0] = color[0] * 
+        if color != gCurrentColor:
+            log.debug("New color is %02x%02x%02x" % (color[0], color[1], color[2]))
+            cmd = "mosquitto_pub -h %s -t ghost/led -m \"LED:%02x%02x%02x\"" % (gMQTTBroker, color[0], color[1], color[2])
+            os.system(cmd)
+            gCurrentColor = color
+
+
+#    print data
+    except Exception as e:
+        log.error("JSON load failed for '%s' : %s" % (msg.payload, e))
+        print traceback.format_exc()
+
+
+def mqttOnPublish(mosq, obj, mid):
+# log.debug("mid: "+str(mid)))
+    pass
+
+
+def mqttOnSubscribe(mosq, obj, mid, granted_qos):
+    log.debug("Subscribed")
+
+
+def mqttOnLog(mosq, obj, level, string):
+    log.debug("log:"+string)
+
+
+def mqttThread():
+    global gQuitting
+    try:
+        mqttc.loop_forever()
+        gQuitting = True
+        log.info("MQTT thread exiting")
+        gQuitting = True
+    except Exception as e:
+        log.error("MQTT thread got exception: %s" % (e))
+        print traceback.format_exc()
+        gQuitting = True
+        log.info("MQTT disconnect")
+        mqttc.disconnect();
+
+def mqttConnect():
+    global mqttc
+    try:
+        # If you want to use a specific client id, use
+        # mqttc = mosquitto.Mosquitto("client-id")
+        # but note that the client id must be unique on the broker. Leaving the client
+        # id parameter empty will generate a random id for you.
+        mqttc = mosquitto.Mosquitto("airlinecolor-%d" % (random.randint(0, 65535)))
+        mqttc.on_message = mqttOnMessage
+        mqttc.on_connect = mqttOnConnect
+        mqttc.on_disconnect = mqttOnDisconnect
+        mqttc.on_publish = mqttOnPublish
+        mqttc.on_subscribe = mqttOnSubscribe
+
+        #mqttc.on_log = mqttOnLog # Uncomment to enable debug messages
+        mqttc.connect(gMQTTBroker, gMQTTPort, 60)
+
+        thread = Thread(target = mqttThread)
+        thread.daemon = True
+        thread.start()
+        return True
+    except socket.error, e:
+        return False
+
+    log.info("MQTT wierdness")
+
+
+def loggingInit():
+    log = logging.getLogger(__name__)
+
+    # Initialize remote logging
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    remotelogger.init(logger = logger, appName = "airlinecol", subSystem = None, host = "midi.local", level = logging.DEBUG)
+
+    if 1:
+        # Log to stdout
+        ch = logging.StreamHandler(sys.stdout)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        ch.setFormatter(formatter)
+        logger.addHandler(ch)
+
+def signal_handler(signal, frame):
+    global gQuitting
+    global mqttc
+    print "ctrl-c"
+    gQuitting = True
+    mqttc.disconnect();
+    sys.exit(0)
+
+def main():
+    global gQuitting
+    global mqttc
+    imagecolor.loadColorData()
+
+    try:
+        signal.signal(signal.SIGINT, signal_handler)
+        loggingInit()
+        log.info("Client started")
+        mqttConnect()
+        while not gQuitting:
+            time.sleep(1)
+    except Exception as e:
+        log.error("Mainloop got exception: %s" % (e))
+        print traceback.format_exc()
+        gQuitting = True
+    log.debug("MQTT disconnect")
+    mqttc.disconnect();
+
+# Ye ol main
+main()
---
+++ imagecolor.py
@@ -0,0 +1,84 @@
+import sys, os
+import urllib2
+import cStringIO
+import json
+from PIL import Image
+
+
+def getProminentColor(searchTerm):
+    if 0:
+        imageUrl = "http://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Wizz_Air_logo.svg/2000px-Wizz_Air_logo.svg.png"
+    #   imageUrl = "http://upload.wikimedia.org/wikipedia/en/thumb/9/9f/United_Parcel_Service_logo.svg/857px-United_Parcel_Service_logo.svg.png"
+    #   imageUrl = "http://upload.wikimedia.org/wikipedia/de/thumb/d/d6/Thomas_Cook_Airlines_Logo.png/150px-Thomas_Cook_Airlines_Logo.png"
+    #   imageUrl = "http://upload.wikimedia.org/wikipedia/commons/d/d4/Logo_Air_Berlin_mit_Claim.jpg"
+    #   imageUrl = "http://commons.wikimedia.org/wiki/File:Tuifly_logo.svg"
+    #   imageUrl = "http://upload.wikimedia.org/wikipedia/commons/thumb/3/33/Scandinavian_Airlines_logo.svg/2000px-Scandinavian_Airlines_logo.svg.png"
+
+    else:
+        searchTerm = searchTerm.replace(" ", "+")
+
+        fetcher = urllib2.build_opener()
+        searchTerm = "%s+logo" % (searchTerm)
+        startIndex = 0
+        searchUrl = "http://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=" + searchTerm + "&start=%d" % (startIndex)
+        f = fetcher.open(searchUrl)
+        data = f.read()
+        j = json.loads(data)
+        for i in range(0, len(j['responseData']['results'])):
+            imageUrl = j['responseData']['results'][i]['unescapedUrl']
+            fileName, fileExtension = os.path.splitext(imageUrl)
+            if fileExtension != ".svg" and fileExtension != ".gif":
+                break
+
+    print imageUrl
+    file = cStringIO.StringIO(urllib2.urlopen(imageUrl).read())
+    im = Image.open(file)
+
+    histogram = {}
+    limit = 10
+
+    px = im.getpixel((0, 0))
+    if isinstance(px, (int, long)):
+        im = im.convert() # Conver to multi-layer image
+
+    for i in range(im.size[0]):
+        for j in range(im.size[1]):
+            px = im.getpixel((i,j))
+#            print px
+            if px != (0, 0, 0) and px != (0, 0, 0, 0) and px != (255, 255, 255):
+                if abs(px[0]-px[1]) > limit or abs(px[0]-px[2]) > limit or abs(px[1]-px[2]) > limit:
+                    if not px in histogram:
+                        histogram[px] = 1
+                    else:
+                        histogram[px] = histogram[px] + 1
+
+    px_max = (0, 0, 0)
+    max_count = 0
+    for px in histogram:
+        if histogram[px] > max_count:
+            px_max = px
+            max_count = histogram[px]
+
+    return (((px_max[0], px_max[1], px_max[2])), imageUrl)
+
+def loadColorData():
+    global colors
+    try:
+        colors = json.load(open("imagecolors.json"))
+    except:
+        colors = {}
+    return colors
+
+def getColor(key):
+    global colors
+    if key in colors:
+        color = colors[key]["color"]
+    else:
+        (color, url) = getProminentColor(key)
+        colors[key] = {}
+        colors[key]["color"] = color
+        colors[key]["url"] = url
+        with open("imagecolors.json", "w+") as f:
+            f.write(json.dumps(colors))
+    return color
+
